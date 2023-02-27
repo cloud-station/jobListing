6ec6241afa49dbf9663793e028a40ca9c4cfbec0
cloud-station <126106017+cloud-station@users.noreply.github.com>
2023-02-27 16:17:27 +0000

    state object list, api link added

--- app.py
+++ app.py
@@ -1,12 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 
 app = Flask(__name__)
 
+STATES = [
+  {
+    'id' : 5,
+    'abbr': 'LA',
+    'capital': 'Ikeja'
+  },
+    {
+    'id' : 2,
+    'abbr': 'FT',
+    'capital': 'Abuja',
+    'geo': 'Federal Capital',
+  },
+    {
+    'id' : 1,
+    'abbr': 'EK',
+    'capital': 'Ekiti'
+  },
+    {
+    'id' : 7,
+    'abbr': 'OS',
+    'capital': 'Osun'
+  },
+    {
+    'id' : 9,
+    'abbr': 'AK',
+    'capital': 'Akwa Ibom'
+  }
+]
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', states=STATES,title = 'State and Capital')
+
+
+@app.route('/api/states')
+def api():
+  return jsonify(STATES)
 
 
 if __name__ == '__main__':
