5f7d9c733b08b9e11c852354e241f1e60614f9ab
cloud-station <126106017+cloud-station@users.noreply.github.com>
2023-02-27 12:52:21 +0000

    requirements.txt added

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello Gen x!</h1>"
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+  app.run(debug=True, port=5000, host='0.0.0.0')
